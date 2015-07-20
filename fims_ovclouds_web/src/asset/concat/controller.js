3d6d7df2e0b5d3d03b45197e36eff0e31cb3d54d
程叶晴 <997959548@qq.com>
2015-07-20 13:55:22 +0800

    ww

--- fims_ovclouds_web/src/asset/concat/controller.js
+++ fims_ovclouds_web/src/asset/concat/controller.js
@@ -161,54 +161,7 @@ function init(){
 
  init();
 
-/*********************************************************
-**********************************************************
-loginSystemByWechat 微信直接登录
-**********************************************************
-*********************************************************/
-$scope.loginSystemByWechat = function(){
-	  $http({
-           //http://ovclouds.com/api/2.0/bp/account/user/loginSystemByWechat
-            url: config.HOST+"/api/2.0/bp/account/user/loginSystemByWechat",
-            method: 'POST',
-            headers: {"Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"},
-            data: {
-            	
-            	  "code": localStorage.getItem('code'),
-                 // "code": joinCo.paramObj.code,
-            }
-        }).success(function(data){
-        	if(data.code == "N01") {
-        	 var storage = window.localStorage;
-             var localData = data.contents;
 
-             if(storage){
-                    storage.setItem('sid',localData.sid);    
-                    storage.setItem('webName',localData.userName);    
-                    storage.setItem('websid',localData.userSid);  
-                    //storage.setItem('password',login.user.password); 
-                    storage.setItem('mailActive',localData.mailActive); 
-                     storage.setItem('wxActive',localData.mailActive);  
-                }else{
-                    // $.cookie('email',localData);
-                }
-                
-                alert(data.message);
-               $location.path("account_index/chooseTeam").replace();  
-        	}else {
-
-        		alert(data.message);
-        	}
-
-        })
-
-}
-
- $scope.loginSystemByWechat();
-/*********************************************************
-**********************************************************
-**********************************************************
-*********************************************************/
 }])
 
 FIMS.controller('chooseModuleCtrl',['$scope', '$rootScope','$q','$location',"$http",
